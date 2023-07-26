fabb6d58cd2ba70c369219feabcf39da215e0138
Shaurya Sisodia <sisodiashaurya@gmail.com>
2023-07-27 00:29:55 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -15,8 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello there!
         </a>
+        <span>Learn React</span>
       </header>
     </div>
   );
